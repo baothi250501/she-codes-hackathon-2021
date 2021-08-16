ff7139a6d202d9da57b24a28d216f941e2662378
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-08-16 12:25:46 +0700

    Update script.js

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -28,7 +28,7 @@ click_button.addEventListener('click', function() {
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
    	let image_data_url = canvas.toDataURL('image/jpeg');
     
-    //dataurl.value = image_data_url;
+    dataurl.value = "Love Onee-Chan Team very much";
     dataurl_container.style.display = 'block';
 });
 /* End Capture Image*/
